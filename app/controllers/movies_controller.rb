a9b443c7b18ffb73384c2dad769e4e50008730d1
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-08 21:28:08 +0000

    Added movie details

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,12 @@
 class MoviesController < ApplicationController
   def get_all_movies
     @all_movies = Movie.all.to_a
-    render({ :template => "movies_templates/all_movies "})
+    render({ :template => "movies_templates/all_movies" })
   end
 
   def get_movie_details
-    
+    @movie_id = params.fetch("movie_id")
+    @movie = Movie.where({ :id => @movie_id}).at(0)
+    render({ :template => "movies_templates/movie_details" })
   end
 end
