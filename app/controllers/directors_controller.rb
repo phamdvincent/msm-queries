639dc171f0026d87302a35f044d354166c26be21
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-08 04:40:00 +0000

    Added starting code and db

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,5 @@
+class DirectorsController < ApplicationController
+  def get_all_directors
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "get_all_directors" })
+  get("/directors/:director_id", { :controller => "directors", :action => "get_director_details"})
+  get("/youngest", { :controller => "directors", :action => "get_youngest_director" })
+  get("/eldest", { :controller => "directors", :action => "get_eldest_director" })
+
+
+  get("/movies", { :controller => "movies", :action => "get_all_movies" })
+  get("/movies/:movie_id", { :controller => "movies", :action => "get_movie_details" })
+
+  get("/actors", { :controller => "actors", :action => "get_all_actors" })
+  get("/actors/:actor_id", { :controller => "actors", :action => "get_actor_details" })
+
 end
